7ab74c72e8fd03e3852067b044946c8d660719aa
devlucas <lucasggarcia27@gmail.com>
2026-03-19 14:00:07 -0300

    Nova home finalizada

--- login.js
+++ login.js
@@ -2,3 +2,10 @@
 // Trabalhando no login
 
 // Finalizando o sistema de login
+
+function login(nome){
+    return nome;
+}
+
+// Sistema de login completo
+// Retornamos o nome do usuário quando logado
